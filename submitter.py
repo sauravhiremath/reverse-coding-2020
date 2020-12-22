0c7a7c7a1fca62b522eb84ee0c157dbc59acb1cf
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-12-22 23:41:49 +0530

    fix: make submitter generic

--- submitter.py
+++ submitter.py
@@ -6,14 +6,14 @@ import json
 url = "https://backend-portal.acmvit.in/judge"
 
 payload = {
-    "problemID": "d4eab63c-492d-49c7-a75e-71030354755d",   
+    "problemID": "d4eab63c-492d-49c7-a75e-71030354755d",   # UUID here
     "teamID": 39,   
-    "language": "py", 
-    "code": "dCA9IGludChpbnB1dCgpKQoKZm9yIF8gaW4gcmFuZ2UodCk6CiAgICBudW0gPSBsaXN0KGJpbihpbnQoaW5wdXQoKSkpKVsyOl0KICAgIAogICAgaWYgbnVtLmNvdW50KCcxJykgJSAyID09IDA6CiAgICAgICAgcHJpbnQoMCkKICAgIGVsc2U6CiAgICAgICAgcHJpbnQoMSk="
+    "language": "py", # cpp, c, java, py
+    "code": "dCA9IGludChpbnB1dCgpKQoKZm9yIF8gaW4gcmFuZ2UodCk6CiAgICBudW0gPSBsaXN0KGJpbihpbnQoaW5wdXQoKSkpKVsyOl0KICAgIAogICAgaWYgbnVtLmNvdW50KCcxJykgJSAyID09IDA6CiAgICAgICAgcHJpbnQoMCkKICAgIGVsc2U6CiAgICAgICAgcHJpbnQoMSk=" #base64 format code
 }
 
 headers = {
-  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtIjp7ImlkIjozOSwibmFtZSI6InBob3NwaG9ydXMifSwicGFydGljaXBhbnQiOnsiZ29vZ2xlSUQiOiIxMDExMjg5NDUwNzU3MDQ5NjU3NDYiLCJuYW1lIjoiU2F1cmF2IEhpcmVtYXRoIiwiaXNBZG1pbiI6dHJ1ZX0sImlhdCI6MTYwODMwMjI5MywiZXhwIjoxNjA4Mzg4NjkzLCJpc3MiOiJoZXBoYWVzdHVzIn0.Kf3ZywktEe8tuSnA9QjNftOKSDOBvUW95GKxhZ6iPW0',
+  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtIjp7ImlkIjozOSwibmFtZSI6InBob3NwaG9ydXMifSwicGFydGljaXBhbnQiOnsiZ29vZ2xlSUQiOiI5OTk5OTk5OTk5OTk5OTk5OSIsIm5hbWUiOiJQaG9zcGhvcnVzIG1lbWJlciIsImlzQWRtaW4iOmZhbHNlfSwiaWF0IjoxNjA4MzAyMjkzLCJleHAiOjE2MDgzODg2OTMsImlzcyI6ImhlcGhhZXN0dXMifQ.dGPFAKEak7r5a5jlVmAyYX_iElT1jdkZrwkzkla-qIo', # signed and verified auth bearer
   'Origin': 'https://reverse-coding-2dff1.web.app',
   'Content-Type': 'application/json',
 }
